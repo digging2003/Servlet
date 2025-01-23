dd7a1ba1f7cd1a242f087ba13e175593207f0235
digging <digging@gmail>
2025-01-23 20:52:41 +0900

    [이지원] - post method 예제 완성 - post method 문제 풀이

--- src/main/java/com/marondal/servlet/test/Test09.java
+++ src/main/java/com/marondal/servlet/test/Test09.java
@@ -3,16 +3,17 @@ package com.marondal.servlet.test;
 import java.io.IOException;
 import java.io.PrintWriter;
 
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+@WebServlet("/servlet/test09")
 public class Test09 extends HttpServlet {
 	
 	@Override
-	public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+	public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
 		
-		response.setCharacterEncoding("utf-8");
 		response.setContentType("text/html");
 		
 		PrintWriter out = response.getWriter();
--- src/main/java/com/marondal/servlet/test/Test10.java
+++ src/main/java/com/marondal/servlet/test/Test10.java
@@ -5,10 +5,12 @@ import java.io.PrintWriter;
 import java.util.HashMap;
 import java.util.Map;
 
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+@WebServlet("/servlet/test10")
 public class Test10 extends HttpServlet {
 	
 	// doPost 메소드 바깥쪽에 위치
@@ -21,7 +23,7 @@ public class Test10 extends HttpServlet {
 	};
 	
 	@Override
-	public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+	public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
 		
 		response.setCharacterEncoding("utf-8");
 		response.setContentType("text/html");
@@ -31,10 +33,30 @@ public class Test10 extends HttpServlet {
 		String id = request.getParameter("id");
 		String password = request.getParameter("password");
 		
-		if(id.equals(userMap.get("id")) && password.equals(userMap.get("password"))) {
+		if(!id.equals(userMap.get("id"))) {
 			out.println(""
 					+ "<html>\n"
-					+ "		<head><title>로그인 성공</title></head>\n");
+					+ "		<head><title>로그인 실패</title></head>\n"
+					+ "		<body>\n"
+					+ "		<p>id가 일치하지 않습니다.</p>\n"
+					+ "		</body>\n"
+					+ "</html>");
+		} else if (!password.equals(userMap.get("password"))) {
+			out.println(""
+					+ "<html>\n"
+					+ "		<head><title>로그인 실패</title></head>\n"
+					+ "		<body>\n"
+					+ "		<p>password가 일치하지 않습니다.</p>\n"
+					+ "		</body>\n"
+					+ "</html>");
+		} else {
+			out.println(""
+					+ "<html>\n"
+					+ "		<head><title>로그인 성공</title></head>\n"
+					+ "		<body>\n"
+					+ "		<h1>" + userMap.get("name") + "님 환영 합니다.</h1>\n"
+					+ "		</body>\n"
+					+ "</html>");
 		}
 		
 	}
